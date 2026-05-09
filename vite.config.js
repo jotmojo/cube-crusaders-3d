2d88b3660da408aa6a33a41c311e71db0006b1fd
Beau Garrett <beau@safehealth.me>
2026-05-08 23:34:08 -0500

    fix railway hosting

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './',
   build: { outDir: 'dist' },
-  server: { port: 3001, open: true }
+  server: { port: 3001, open: true },
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3001,
+    host: true,
+    allowedHosts: 'all',
+  }
 });
