504909e0a8c6cb43dc8d803468d997043bbcf35e
Beau Garrett <beau@safehealth.me>
2026-05-09 01:21:49 -0500

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './',
   build: { outDir: 'dist' },
-  server: { port: 3001, open: true },
+  server: { port: 3001, open: false },
   preview: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3001,
-    host: true,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    host: '0.0.0.0',
     allowedHosts: 'all',
+    open: false,
   }
 });
